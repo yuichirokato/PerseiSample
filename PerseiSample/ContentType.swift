14d23cb00e23c8831cee656492a2a1228a7a4826
takahashi <ikmy0709@gmail.com>
2015-08-22 01:13:57 +0900

    initial commit

---
+++ PerseiSample/ContentType.swift
@@ -0,0 +1,13 @@
+//
+//  ContentType.swift
+//  PerseiSample
+//
+//  Created by yuichiro_t on 2015/08/22.
+//  Copyright (c) 2015年 yuichiro_t. All rights reserved.
+//
+
+import UIKit
+
+class ContentType: NSObject {
+   
+}
